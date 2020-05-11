e233b84efe20ca12cf2cea07d01e0d200d8dd91c
phamdat16041999 <56620800+phamdat16041999@users.noreply.github.com>
2020-05-11 13:03:53 +0700

    Update MainController.java

--- src/main/java/springlogin/controller/MainController.java
+++ src/main/java/springlogin/controller/MainController.java
@@ -114,7 +114,7 @@ public class MainController {
     public String manageDuantrienkhai(Model model) {
 		List<duantrienkhai> duantrienkhai = duantrienkhaiRepository.findAll();
 		model.addAttribute("Duantrienkhai", duantrienkhai);
-		return "/admin/duantrienkhai";
+		return "admin/duantrienkhai";
 		
 	}
     @RequestMapping("/viewUpdateDuan/{id}")
@@ -130,7 +130,7 @@ public class MainController {
     	duantrienkhaiRepository.save(Duantrienkhai);
 		List<duantrienkhai> duantrienkhai = duantrienkhaiRepository.findAll();
 		model.addAttribute("Duantrienkhai", duantrienkhai);
-		return "/admin/duantrienkhai";
+		return "admin/duantrienkhai";
     	
     }
     @RequestMapping("/viewAddDuAn")
@@ -143,7 +143,7 @@ public class MainController {
     	duantrienkhaiRepository.save(Duantrienkhai);
 		List<duantrienkhai> duantrienkhai = duantrienkhaiRepository.findAll();
 		model.addAttribute("Duantrienkhai", duantrienkhai);
-		return "/admin/duantrienkhai";
+		return "admin/duantrienkhai";
     	
     }
     @RequestMapping("/deleteDuAn/{id}")
@@ -151,20 +151,20 @@ public class MainController {
     	duantrienkhaiRepository.deleteById(id);
 		List<duantrienkhai> duantrienkhai = duantrienkhaiRepository.findAll();
 		model.addAttribute("Duantrienkhai", duantrienkhai);
-		return "/admin/duantrienkhai";
+		return "admin/duantrienkhai";
     	
     }
     @RequestMapping("manageLienHe")
     public String manageLienHe(Model model){
 //		List<lienhe> lienhe = LienheRepository.findAll();
 //		model.addAttribute("Lienhe", lienhe);
-		return "/admin/lienhe";
+		return "admin/lienhe";
     }
     @RequestMapping(value = "searchLienHe", produces = "application/x-www-form-urlencoded;charset=UTF-8")
     public String searchLienHe(@RequestParam(value = "searchKey") String searchKey, Model model){
     	List<lienhe> lienhe = (List<springlogin.entities.lienhe>) LienheRepository.searchLienHe(searchKey);
 	    model.addAttribute("Lienhe", lienhe);
-	    return "/admin/lienhe";
+	    return "admin/lienhe";
     
     
     }
@@ -172,7 +172,7 @@ public class MainController {
     public String manageAccount(Model model) {
 		List<account> account = AccountRepository.findAll();
 		model.addAttribute("Account", account);
-		return "/admin/selectaccount";
+		return "admin/selectaccount";
     }
     @RequestMapping("viewAddAccount")
     public String viewAddAccount(Model model) {
@@ -199,7 +199,7 @@ public class MainController {
 		
    		
 		  modelAndView.addObject("Account",AccountRepository.findAll());
-          modelAndView.setViewName("/admin/selectaccount");
+          modelAndView.setViewName("admin/selectaccount");
           return modelAndView;
     }
     @RequestMapping("/deleteAccount/{id}")
@@ -207,7 +207,7 @@ public class MainController {
     	AccountRepository.deleteById(id);
    		List<account> account = AccountRepository.findAll();
 		model.addAttribute("Account", account);
-		return "/admin/selectaccount";
+		return "admin/selectaccount";
     	
     }
     @RequestMapping("/viewUpdateAccount/{id}")
@@ -224,7 +224,7 @@ public class MainController {
    		AccountRepository.save(Account);
    		List<account> account = AccountRepository.findAll();
 		model.addAttribute("Account", account);
-		return "/admin/selectaccount";
+		return "admin/selectaccount";
     }
     
     
@@ -237,7 +237,7 @@ public class MainController {
     @RequestMapping(value = "/logoutSuccessful", method = RequestMethod.GET)
     public String logoutSuccessfulPage(Model model) {
         model.addAttribute("title", "Logout");
-        return "/index";
+        return "index";
     }
     @RequestMapping(value = "/403", method = RequestMethod.GET)
     public String accessDenied(Model model, Principal principal) {
