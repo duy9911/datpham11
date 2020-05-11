445676031b17138237444f0b32d7d01f54107eb8
phamdat16041999 <56620800+phamdat16041999@users.noreply.github.com>
2020-05-11 13:37:43 +0700

    Update appUserRepository.java

--- src/main/java/springlogin/Repository/appUserRepository.java
+++ src/main/java/springlogin/Repository/appUserRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.query.Param;
 
 import springlogin.entities.AppUser;
 
